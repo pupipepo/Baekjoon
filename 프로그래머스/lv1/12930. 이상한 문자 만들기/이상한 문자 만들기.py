b0900fa7e4c0531576de538d0bdfbfaa42ad39e0
pupipepo <128676469+pupipepo@users.noreply.github.com>
2023-04-25 13:23:38 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ''
+    cursor = 0
+    for i in s:
+        if i==' ':
+            answer+=' '
+            cursor=0
+        else:
+            cursor +=1
+            if cursor%2==1:
+                answer+=i.upper()
+            else:
+                answer+=i.lower()
+    return answer
